1f1e97da259869c1e91fd87040f7394ff86a11a7
Teguh Ardiansyah <teguhmublk6@gmail.com>
2025-10-17 08:48:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6423259316-a6e62",
+  "appId": "1:319987182775:web:1982705f254df587d3b132",
+  "apiKey": "AIzaSyD0Zp4xP3Jzu5mbme8Hd66lkOnd8SjyKis",
+  "authDomain": "studio-6423259316-a6e62.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "319987182775"
+};
