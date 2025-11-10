ccb71067271e441507c05888ed75b46327f90494
Teguh Ardiansyah <teguhmublk6@gmail.com>
2025-11-10 22:16:26 +0000

    Pada bagian return kamu selalu dan tetap menyertakan html dan body ,apa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
-import { FirebaseClientProvider } from '@/firebase'
+import { FirebaseClientProvider } from '@/firebase/client-provider'
 import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata: Metadata = {
