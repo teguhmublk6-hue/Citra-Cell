078e11218687df9a76d486c52617cee0c69c437f
Teguh Ardiansyah <teguhmublk6@gmail.com>
2025-11-10 21:58:05 +0000

    Error yang sama masih muncul setelah npm run Build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 
-
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
@@ -35,5 +34,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-    
