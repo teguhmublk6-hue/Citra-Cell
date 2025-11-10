d1a7576ba77241193dc4444c11f894671abb27e3
Teguh Ardiansyah <teguhmublk6@gmail.com>
2025-11-10 17:41:10 +0000

    Lalu di bawah itu apa ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
@@ -34,3 +35,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+    
