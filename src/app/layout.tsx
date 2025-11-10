dd437b3b1785f4fbd87732bae54b4f4a014f2cb3
Teguh Ardiansyah <teguhmublk6@gmail.com>
2025-11-10 17:31:52 +0000

    Yakin ? Coba cek lagi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,23 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body antialiased bg-background text-foreground">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <FirebaseClientProvider>
-            {children}
-          </FirebaseClientProvider>
-        </ThemeProvider>
-        <Toaster />
-      </body>
-    </html>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <FirebaseClientProvider>
+        {children}
+      </FirebaseClientProvider>
+    </ThemeProvider>
   );
 }
