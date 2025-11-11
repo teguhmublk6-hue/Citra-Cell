33b538301f6665f2fa088f0c391ca99ee535cf55
Teguh Ardiansyah <teguhmublk6@gmail.com>
2025-11-11 11:31:17 +0000

    Buatkan saya langkah untuk membungkus app ini dengan detail

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.brimo.app',
+  appName: 'Brimo App',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -60,7 +62,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https://images.seeklogo.com',
         hostname: 'images.seeklogo.com',
         port: '',
         pathname: '/**',
@@ -90,7 +92,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https://play-lh.googleusercontent.com',
         hostname: 'play-lh.googleusercontent.com',
         port: '',
         pathname: '/**',
